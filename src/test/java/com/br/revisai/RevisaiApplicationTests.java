943d828e20e966ef5449d39a26e4b28abafc666a
Kaua Breno <kauabrenojobs@gmail.com>
2025-10-14 12:23:36 -0300

    first commit

---
+++ src/test/java/com/br/revisai/RevisaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.revisai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
